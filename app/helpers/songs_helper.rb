962fc1b0cef69a6b61765d7431808af4467bef6a
Victoria Meng <nights1ststar@protonmail.com>
2018-07-24 17:23:07 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,8 +1,8 @@
 module SongsHelper
   def display_artist
     if artist_name
-      link_to(artist_name, self.artist) 
-    else 
+      link_to(artist_name, self.artist)
+    else
       link_to("Add Artist")
     end
   end
